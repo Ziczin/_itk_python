37b2a7b830b89a71451f6c3d6346d326455af49f
Корнеев Александр Сергеевич <KorneevAS@sood.ru>
2026-01-15 11:08:26 +0400

    mod3 adv task
    Добавил модуль orjson, должен работать быстрее стандартного, вынес обработку из основного потока через run_in_executor

--- src/module_3/tasks/a_http_advanced.py
+++ src/module_3/tasks/a_http_advanced.py
@@ -1,24 +1,34 @@
 import asyncio
 import aiohttp
-import json
+import orjson
 from aiohttp import ClientTimeout
+from concurrent.futures import ProcessPoolExecutor
+from multiprocessing import cpu_count
 
 
-async def fetch_single_url(session, url, semaphore):
+async def fetch_single_url(session, url, semaphore, loop, pool_executor):
     async with semaphore:
         try:
             print(f"Processing: {url}")
             timeout = ClientTimeout(total=60, connect=10, sock_read=30)
+
             async with session.get(url, timeout=timeout) as response:
                 response.raise_for_status()
-                data = await response.json()
+                text = response.text()
+
+                with pool_executor as pool:
+                    data = await loop.run_in_executor(pool, orjson.loads, text)
+
                 return {"url": url, "data": data}
+
         except asyncio.TimeoutError:
             print(f"Timeout for: {url}")
             return None
+
         except aiohttp.ClientError as e:
             print(f"Client error for: {url}: {e}")
             return None
+
         except Exception as e:
             print(f"Error for: {url}: {e}")
             return None
@@ -29,6 +39,9 @@ async def main():
     output_file = "result.jsonl"
     max_concurrent = 5
 
+    loop = asyncio.get_event_loop()
+    pool_executor = ProcessPoolExecutor(max_workers=cpu_count())
+
     semaphore = asyncio.Semaphore(max_concurrent)
 
     async with aiohttp.ClientSession() as session:
@@ -37,7 +50,9 @@ async def main():
 
         tasks = []
         for url in urls:
-            task = asyncio.create_task(fetch_single_url(session, url, semaphore))
+            task = asyncio.create_task(
+                fetch_single_url(session, url, semaphore, loop, pool_executor)
+            )
             tasks.append(task)
 
         with open(output_file, "w") as out_f:
@@ -46,8 +61,9 @@ async def main():
                     result = await asyncio.wait_for(task, timeout=70)
                     if result:
                         json_line = {result["url"]: result["data"]}
-                        out_f.write(json.dumps(json_line, ensure_ascii=False) + "\n")
+                        out_f.write(orjson.dumps(json_line, ensure_ascii=False) + "\n")
                         out_f.flush()
+
                 except asyncio.TimeoutError:
                     continue
 
